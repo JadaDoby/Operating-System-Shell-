bff24fe3d00861534510d8b963eb5d52cb1a4026
JadaDoby <139698683+JadaDoby@users.noreply.github.com>
2024-02-02 18:16:09 -0500

    Update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -135,3 +135,159 @@ void pipingcommand(tokenlist *tokens)
     free_tokens(cmd2);
 }
 
+
+
+
+void pipingcommand(tokenlist *tokens)
+{
+    bool isBgProcess = false;
+    pid_t pid;
+    int status;
+
+    int pipes[3][2];  // Three pipes for three commands
+    tokenlist *cmd1 = new_tokenlist();
+    tokenlist *cmd2 = new_tokenlist();
+    tokenlist *cmd3 = new_tokenlist();
+
+    // Find the indices of the pipe symbols
+    int pipeIndices[2] = {0, 0};
+    for (int i = 0; i < tokens->size; i++)
+    {
+        if (strcmp(tokens->items[i], "|") == 0)
+        {
+            if (pipeIndices[0] == 0)
+            {
+                pipeIndices[0] = i;
+            }
+            else
+            {
+                pipeIndices[1] = i;
+                break;
+            }
+        }
+        add_token(cmd1, tokens->items[i]);
+    }
+
+    // Populate cmd2 with tokens between the first and second pipe symbols
+    for (int i = pipeIndices[0] + 1; i < pipeIndices[1]; i++)
+    {
+        add_token(cmd2, tokens->items[i]);
+    }
+
+    // Populate cmd3 with tokens after the second pipe symbol
+    for (int i = pipeIndices[1] + 1; i < tokens->size; i++)
+    {
+        add_token(cmd3, tokens->items[i]);
+    }
+
+    // Create pipes
+    if (pipe(pipes[0]) == -1 || pipe(pipes[1]) == -1 || pipe(pipes[2]) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    // First child process
+    pid = fork();
+    if (pid == 0)
+    {
+        // Close read ends of the pipes
+        close(pipes[0][0]);
+        close(pipes[1][0]);
+        close(pipes[1][1]);
+        close(pipes[2][0]);
+        close(pipes[2][1]);
+
+        // Redirect stdout to the write end of the first pipe
+        dup2(pipes[0][1], STDOUT_FILENO);
+
+        // Close unused file descriptors
+        close(pipes[0][1]);
+
+        // Execute cmd1
+        Execute_Command(cmd1, isBgProcess);
+
+        // Exit child process
+        exit(EXIT_SUCCESS);
+    }
+
+    // Second child process
+    pid = fork();
+    if (pid == 0)
+    {
+        // Close read end of the first pipe and write end of the second pipe
+        close(pipes[0][1]);
+        close(pipes[1][0]);
+        close(pipes[2][0]);
+        close(pipes[2][1]);
+
+        // Redirect stdin from the read end of the first pipe
+        dup2(pipes[0][0], STDIN_FILENO);
+
+        // Close read end of the first pipe
+        close(pipes[0][0]);
+
+        // Redirect stdout to the write end of the second pipe
+        dup2(pipes[1][1], STDOUT_FILENO);
+
+        // Close unused file descriptors
+        close(pipes[1][1]);
+
+        // Execute cmd2
+        Execute_Command(cmd2, isBgProcess);
+
+        // Exit child process
+        exit(EXIT_SUCCESS);
+    }
+
+    // Third child process
+    pid = fork();
+    if (pid == 0)
+    {
+        // Close read end of the second pipe
+        close(pipes[0][0]);
+        close(pipes[1][1]);
+        close(pipes[2][0]);
+
+        // Redirect stdin from the read end of the second pipe
+        dup2(pipes[1][0], STDIN_FILENO);
+
+        // Close read end of the second pipe
+        close(pipes[1][0]);
+
+        // Execute cmd3
+        Execute_Command(cmd3, isBgProcess);
+
+        // Exit child process
+        exit(EXIT_SUCCESS);
+    }
+
+    // Close unused file descriptors in the parent process
+    close(pipes[0][0]);
+    close(pipes[0][1]);
+    close(pipes[1][0]);
+    close(pipes[1][1]);
+    close(pipes[2][1]);
+
+    // Wait for all child processes to finish
+    waitpid(-1, &status, 0);
+
+    // Close read end of the third pipe
+    close(pipes[2][0]);
+
+    // Read and print the output of the last command
+    char buffer[1024];
+    ssize_t bytesRead;
+    while ((bytesRead = read(pipes[2][0], buffer, sizeof(buffer))) > 0)
+    {
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
+
+    // Close the read end of the third pipe
+    close(pipes[2][0]);
+
+    // Free memory
+    free_tokens(cmd1);
+    free_tokens(cmd2);
+    free_tokens(cmd3);
+}
