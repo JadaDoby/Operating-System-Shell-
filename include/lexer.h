e3ba2c88ba7e612401d5ab5b9c7c503166f4e11b
SSJROOD <137559489+SSJROOD@users.noreply.github.com>
2024-02-03 12:08:52 -0500

    Update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -18,7 +18,7 @@ typedef struct
 typedef struct
 {
     pid_t pid;
-    char ***command;
+    char **command;
 } BackgroundProcess;
 
 char *get_input(void);
@@ -27,7 +27,7 @@ tokenlist *new_tokenlist(void);
 void add_token(tokenlist *tokens, char *item);
 void free_tokens(tokenlist *tokens);
 void Execute_Command(tokenlist *tokens, bool isBgProcess);
-void addBGProcess(pid_t pid, const char **command, BackgroundProcess *bgProcesses);
+void addBGProcess(pid_t pid,  char **command, BackgroundProcess *bgProcesses);
 void BackgroundProcessHelper(BackgroundProcess *bgProcesses);
 void jobsCommand(bool background);
 char *getPathSearch(tokenlist *cmd);
@@ -35,4 +35,4 @@ char *expand_tilde(const char *token);
 void expand_tildeHelper(tokenlist *tokens);
 void ioRedirection(tokenlist *tokens,bool isBgProcess);
 tokenlist *expandEnvVariables(tokenlist *tokens);
-
+void piping(tokenlist *tokens, bool isBgProcess);
